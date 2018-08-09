b2ee22a9faf1a136cc7f1ff48d0dd70daea5b7c4
TJ Bearse <thomasb@qualtrics.com>
2018-08-08 22:23:59 -0700

    Building hex map tiles from svg

--- Makefile
+++ Makefile
@@ -2,10 +2,18 @@
 start:
 	serve
 
-layers:
+layers: mapParts/exports/zoom1-0.0-16x16.png
 	mkdir -p layers
-	bash layers.sh
+	./tileMap.py
+
+mapParts/exports/%.png: mapParts/%.svg | mapParts/exports
+	inkscape -f $< -C -z -e $@
+
+mapParts/exports:
+	mkdir -p $@
 
 .PHONY: clean
 clean:
 	rm -r layers
+	rm -r temp
+	rm -r mapParts/exports
--- map.js
+++ map.js
@@ -1,14 +1,41 @@
-var tileSize = 256; // zero is half on all sides
+var tileSize = 256;
+
+var MySimple = L.Util.extend({}, L.CRS.Simple, {
+    // TODO may be able to replace this with a simple js map, or a step function
+    scale: function (zoom) {
+	/*
+	if (zoom >= 4) {
+	    return Math.pow(2, 2*4+zoom-4);
+	} else {
+	    return Math.pow(2, 2*zoom);
+	}
+	*/
+	return Math.pow(2, zoom);
+    },
+    zoom: function (scale) {
+	/*
+	if (scale <= 256) {
+	    return Math.log(scale) / (Math.LN2 * 2);
+	} else {
+	    return (Math.log(scale) - 8 * Math.LN2) / Math.LN2;
+	}
+	*/
+	return Math.log(scale) / (Math.LN2);
+    },
+});
+
+
+
 var map = L.map('map', {
     // TODO setup size and wrapping
     // may need my own crs? https://leafletjs.com/reference-1.3.2.html#crs-wraplng
     // this would also allow for skipping zoom layers / customizing the level
-    crs: L.CRS.Simple,
-}).setView([0, 0], 13);
+    crs: MySimple
+}).setView([-1*tileSize, 1*tileSize], 0);
 var imgUrl = 'tile.png';
 var tileLayer = L.tileLayer('layers/{z}/{x}.{y}.png', {
     // determined by how much we generate
-    bounds: [[-3 * tileSize, -2 * tileSize], [2 * tileSize, 3 * tileSize]], // zero is extra
+    bounds: [[-2*tileSize,0], [0, 2*tileSize]],
     minZoom: 0,
-    maxZoom: 1
+    maxZoom: 3
 }).addTo(map);
